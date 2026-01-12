4651902f9013bde1c039f4cbb7c6a367c2b34fb1
PaintThePicture <alex.edemalm15@gmail.com>
2026-01-12 17:47:31 +0100

    feat(timeEditCtrl): endpoint, courseId pathparam, supported

--- ltu_schedule_manager/src/main/java/com/github/api/controllers/TimeEditCtrl.java
+++ ltu_schedule_manager/src/main/java/com/github/api/controllers/TimeEditCtrl.java
@@ -1,14 +1,34 @@
 package com.github.api.controllers;
 
 import com.github.api.RestApiRoutable;
+import com.github.api.integration.timeedit.TimeEditClient;
 
 import io.javalin.Javalin;
 
 public class TimeEditCtrl implements RestApiRoutable {
 
+    private final TimeEditClient teClient = new TimeEditClient();
+    private String teClientURL; 
+
     @Override
     public void registerEndpoints(Javalin app) {
+        
+        app.get("/api/time-edit/courses/{courseId}/schedule", ctx -> {
 
+            String courseId = ctx.pathParam("courseId");
+
+            var schedule = teClient.searchAndGetSchedule(courseId);
+
+            ctx.json(schedule);
+        });
+
+        app.get("/api/time-edit/fetch/{url}/schedule", ctx -> {
+
+            String courseId = ctx.pathParam("url");
+
+        });
     }
+
+
     
 }
