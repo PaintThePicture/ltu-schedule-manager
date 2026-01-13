cf721d85e3963bfe849d1db56235e7d7b9094e48
PaintThePicture <alex.edemalm15@gmail.com>
2026-01-13 14:00:04 +0100

    feat(WebClient): new post feature, shared sendAsync method

--- ltu_schedule_manager/src/main/java/com/github/utilities/WebClient.java
+++ ltu_schedule_manager/src/main/java/com/github/utilities/WebClient.java
@@ -8,7 +8,19 @@ import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
 interface GenericHttpClient {
-    CompletableFuture<String> fetchAsync(String url);
+    CompletableFuture<String> getAsync(String url);
+    CompletableFuture<String> postAsync(String url, String token, String jsonBody);
+
+
+    default CompletableFuture<String> sendAsync(HttpClient httpClient, HttpRequest request) {
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                                                    .thenApply(response -> {
+                                                    if (response.statusCode() >= 400) {
+                                                        throw new RuntimeException("Error: http " + response.statusCode());
+                                                    }
+                                                    return response.body();
+                                                    });
+    }
     
 }
 
@@ -16,28 +28,33 @@ public class WebClient implements GenericHttpClient {
     private static final WebClient INSTANCE = new WebClient();
     private final HttpClient httpClient;
     
-    private WebClient() {
-        this.httpClient = HttpClient.newBuilder()
-                                    .connectTimeout(Duration.ofSeconds(8))
-                                    .followRedirects(HttpClient.Redirect.NORMAL)
-                                    .build();
-    }
-    
-
     @Override
-    public CompletableFuture<String> fetchAsync(String url) {
+    public CompletableFuture<String> getAsync(String url) {
         HttpRequest request = HttpRequest.newBuilder()
                                          .uri(URI.create(url))
                                          .GET()
                                          .build();
 
-        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                                                         .thenApply(response -> {
-                                                            if (response.statusCode() >= 400) {
-                                                                throw new RuntimeException("Error: http " + response.statusCode());
-                                                            }
-                                                            return response.body();
-                                                         });
+        return sendAsync(httpClient, request);
+    }
+
+    @Override
+    public CompletableFuture<String> postAsync(String url, String token, String jsonBody) {
+        HttpRequest request = HttpRequest.newBuilder()
+                                         .uri(URI.create(url))
+                                         .header("Authorization", "Bearer " + token)
+                                         .header("Content-Type", "application/json")
+                                         .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                                         .build();
+
+        return sendAsync(httpClient, request);
+    }
+
+    private WebClient() {
+        this.httpClient = HttpClient.newBuilder()
+                                    .connectTimeout(Duration.ofSeconds(8))
+                                    .followRedirects(HttpClient.Redirect.NORMAL)
+                                    .build();
     }
 
     public static WebClient getInstance() {
