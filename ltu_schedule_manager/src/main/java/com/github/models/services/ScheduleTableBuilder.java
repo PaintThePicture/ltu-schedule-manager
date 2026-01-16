800dee0ed787142ad0af6cb3d59a6dbd1cc36121
PaintThePicture <alex.edemalm15@gmail.com>
2026-01-16 12:59:07 +0100

    docs(Builder): comment code

--- ltu_schedule_manager/src/main/java/com/github/models/services/ScheduleTableBuilder.java
+++ ltu_schedule_manager/src/main/java/com/github/models/services/ScheduleTableBuilder.java
@@ -7,10 +7,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-
+/**
+ * Builder for configuring a TableView to display TimeEdit reservations.
+ */
 public class ScheduleTableBuilder {
     private final TableView<TimeEditReservation> table; 
 
+    /**
+     * Constructs a ScheduleTableBuilder with the specified builder.
+     * @param builder
+     */
     private ScheduleTableBuilder(Builder builder) {
         this.table = builder.table;
 
@@ -18,9 +24,19 @@ public class ScheduleTableBuilder {
         this.table.setItems(ScheduleStore.getInstance().getReservations());
     }
 
+    /**
+     * Builder class for ScheduleTableBuilder.
+     */
     public static class Builder {
         private final TableView<TimeEditReservation> table;
 
+        /**
+         * Constructor for Builder.
+         * @param table
+         * @param colSelect
+         * @param dateCol
+         * @param timeCol
+         */
         public Builder(TableView<TimeEditReservation> table,
                        TableColumn<TimeEditReservation, Boolean> colSelect,
                        TableColumn<TimeEditReservation, String> dateCol, 
@@ -35,19 +51,39 @@ public class ScheduleTableBuilder {
 
             dateCol.setCellValueFactory(new PropFactory<>("displayDate"));
             timeCol.setCellValueFactory(new PropFactory<>("displayTimeRange"));
-        }
+        }  
+        /**
+         * Adds activity column configuration.
+         * @param col
+         * @return
+         */
         public Builder withActivity(TableColumn<TimeEditReservation, String> col) {
             col.setCellValueFactory(new PropFactory<>("activity"));
             return this;
         }
+        /**
+         * Adds location column configuration.
+         * @param col
+         * @return
+         */
         public Builder withLocation(TableColumn<TimeEditReservation, String> col) {
             col.setCellValueFactory(new PropFactory<>("location"));
             return this;
-        }
+        }  
+        /**
+         * Adds course code column configuration.
+         * @param col
+         * @return
+         */
         public Builder withCourseCode(TableColumn<TimeEditReservation, String> col) {
             col.setCellValueFactory(new PropFactory<>("courseCode"));
             return this;
         }
+        /**
+         * Adds comment column configuration.
+         * @param col
+         * @return
+         */
         public Builder withComment(TableColumn<TimeEditReservation, String> col) {
             col.setCellValueFactory(new PropFactory<>("userComment"));
             
@@ -55,12 +91,15 @@ public class ScheduleTableBuilder {
             col.setEditable(true);
             return this;
         }
-
+        /**
+         * Builds the ScheduleTableBuilder.
+         * @return
+         */
         public ScheduleTableBuilder build() {
             return new ScheduleTableBuilder(this);
         }
     }
-
+    // Helper class for property value factories.
     private static class PropFactory<T> extends PropertyValueFactory<TimeEditReservation, T> {
         public PropFactory(String property) {
             super(property);
--- ltu_schedule_manager/src/main/java/com/github/utilities/AltertWindowBuilder.java
+++ ltu_schedule_manager/src/main/java/com/github/utilities/AltertWindowBuilder.java
@@ -9,9 +9,16 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import javafx.stage.Window;
 
+/**
+ * Builder for creating alert windows.
+ */
 public class AltertWindowBuilder {
     private final Alert alert;
 
+    /**
+     * Constructs an AltertWindowBuilder with the specified builder.
+     * @param builder
+     */
     private AltertWindowBuilder(Builder builder) {
         this.alert = new Alert(builder.type);
         this.alert.setTitle(builder.title);
@@ -32,7 +39,9 @@ public class AltertWindowBuilder {
 
         this.alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
     }
-
+    /**
+     * Builder class for AltertWindowBuilder.
+     */
     public static class Builder {
         private final AlertType type;
         private final String title;
@@ -41,29 +50,53 @@ public class AltertWindowBuilder {
         private String header;
         private Throwable exception;
         private Window owner;
-
+        /**
+         * Constructor for Builder.
+         * @param type
+         * @param title
+         * @param content
+         */
         public Builder(AlertType type, String title, String content) {
             this.type = type;
             this.title = title;
             this.content = content;
         }
+        /**
+         * Sets the header text.
+         * @param header
+         * @return
+         */
         public Builder withHeader(String header) {
             this.header = header;
             return this;
         }
+        /**
+         * Sets the exception to display stack trace.
+         * @param e
+         * @return
+         */
         public Builder withException(Throwable e) {
             this.exception = e;
             return this;
         }
+        /**
+         * Sets the owner window.
+         * @param owner
+         * @return
+         */
         public Builder withOwner(Window owner) {
             this.owner = owner;
             return this;
         }
+        /**
+         * Builds the AltertWindowBuilder.
+         * @return
+         */
         public AltertWindowBuilder build() {
             return new AltertWindowBuilder(this);
         }
     }
-
+    // Creates a TextArea node containing the stack trace of the exception.
     private TextArea createStackTraceNode(Throwable e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
@@ -76,7 +109,9 @@ public class AltertWindowBuilder {
             textArea.setMaxHeight(Double.MAX_VALUE);
             return textArea;
     }
-
+    /**
+     * Displays the alert window.
+     */
     public void show() {
         this.alert.showAndWait();
     }
