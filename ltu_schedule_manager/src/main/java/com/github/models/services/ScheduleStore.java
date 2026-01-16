d9b57dcb7834ad978b75ed27312bd4f5fbe33b4d
PaintThePicture <alex.edemalm15@gmail.com>
2026-01-16 12:59:24 +0100

    docs(sechulestore) comment code

--- ltu_schedule_manager/src/main/java/com/github/models/services/ScheduleStore.java
+++ ltu_schedule_manager/src/main/java/com/github/models/services/ScheduleStore.java
@@ -10,6 +10,9 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+/**
+ * Singleton store for managing schedule reservations.
+ */
 public class ScheduleStore {
     private static final ScheduleStore INSTANCE = new ScheduleStore();
 
@@ -19,34 +22,54 @@ public class ScheduleStore {
 
     private ScheduleStore() {}
 
+    /**
+     * Gets the observable list of reservations.
+     * @return
+     */
     public ObservableList<TimeEditReservation> getReservations() {
         return reservations;
     }
 
+    /**
+     * Gets the list of selected reservations.
+     * @return
+     */
     public ObservableList<TimeEditReservation> getSelectedReservations(){
         return reservations.stream()
                            .filter(TimeEditReservation::isSelected)
                            .collect(Collectors.toCollection(FXCollections::observableArrayList));
     }
-    
+    /**
+     * Gets the list of selected reservations as a standard list.
+     * @return
+     */
     public List<TimeEditReservation> getSelectedReservationsToList(){
         return reservations.stream()
                            .filter(TimeEditReservation::isSelected)
                            .toList();
     }
-
+    /**
+     * Loads schedule for the specified course ID.
+     * @param courseId
+     */
     public void loadCourseSchedule(String courseId) {
         client.getScheduleByCourse(courseId).thenAccept(newList -> {
             Platform.runLater(() -> reservations.setAll(newList));
         });
     }
-
+    /**
+     * Imports reservations from the specified URL.
+     * @param url
+     */
     public void importFromUrl(String url) {
         client.fetchFromApi(url).thenAccept(newList -> {
             Platform.runLater(() -> reservations.setAll(newList));
         });
     }
-
+    /**
+     * Gets the singleton instance of ScheduleStore.
+     * @return
+     */
     public static ScheduleStore getInstance() {
         return INSTANCE;
     }
